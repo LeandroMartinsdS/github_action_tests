59d88f69c4fe7635fab9adc90ae22cce8b46657c
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2025-05-09 17:44:09 +0100

    Add clean version

---
+++ test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+// #define RUN_AS_RT_APP
+// #define DEBUG
+
+#include <gplib.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define _PPScriptMode_		// for enum mode, replace this with #define _EnumMode_
+#include "../../Include/pp_proj.h"
+
+
+#endif // TEST_H
